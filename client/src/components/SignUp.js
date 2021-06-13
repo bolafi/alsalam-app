2b91467b3777f0986e6bbf0fd78b64acf5589f71
bolafi <khalidzdg@gmail.com>
2021-06-13 21:19:29 +0300

    modifiy the register url

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -93,7 +93,7 @@ const useStyles = makeStyles((theme) => ({
 
 const registerUser = async (userData) => {
   try {
-    const data = await fetch("http://localhost:5000/api/users", {
+    const data = await fetch("/api/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
