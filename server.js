53968347ae3d5974e3b12608b2e0dfae4d57fe9a
khaled alafifi <>
2022-10-10 11:04:26 +0300

    commit the helmet

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 
 // Set Security headers
-app.use(helmet({ contentSecurityPolicy: false }));
+//app.use(helmet({ contentSecurityPolicy: false }));
 
 // Prevent xss attacks
 //app.use(xss());
